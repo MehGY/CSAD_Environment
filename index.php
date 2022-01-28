fef1fc281fded54245b95601529ef676064aeb47
MehGY <87634925+MehGY@users.noreply.github.com>
2022-01-28 18:28:40 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <header>
+        <title>CSAD</title>
+        <link link rel="stylesheet" type="text/css" href="css/style.css">
+    </header>
+        <?php include 'header.html'; ?>
+        <div>
+            <p class="title">MAMAMIYA MARIO</p>
+            <p class="details">abisdb asiduabsd aosudubabkefvbwn jasbkdanbsdn asiudbkasbdnasbd asbdkajbs</p>
+        </div>
+        <div>
+            <p class="title">AHHHHHHHHHHHHHHHHHH</p>
+            <p class="details">abisdb asiduabsd aosudubabkefvbwn jasbkdanbsdn asiudbkasbdnasbd asbdkajbs KASBDKABS AJb a sduahvJHVJas dJ asjhJB jasjd jhabsd nashdhba</p>
+        </div>
+    
+        <?php include 'signUp.php'; ?>
+    </body>
+    <?php include 'footer.html'; ?>
+</html>
+
