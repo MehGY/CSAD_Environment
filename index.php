b787bc7dbfafa2622e16a38117e819e2dee5a7e6
MehGY <87634925+MehGY@users.noreply.github.com>
2022-02-01 17:47:16 +0800

    Update 1.1

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
-<html>
+<html lang="en">
     <header>
         <title>CSAD</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     </header>
         <?php include 'header.html'; ?>
         <div>
-            <p class="title">MAMAMIYA MARIO</p>
+            <h2 class="title">MAMAMIYA MARIO</h2>
             <p class="details">abisdb asiduabsd aosudubabkefvbwn jasbkdanbsdn asiudbkasbdnasbd asbdkajbs</p>
         </div>
         <div>
-            <p class="title">AHHHHHHHHHHHHHHHHHH</p>
+            <h2 class="title">AHHHHHHHHHHHHHHHHHH</h2>
             <p class="details">abisdb asiduabsd aosudubabkefvbwn jasbkdanbsdn asiudbkasbdnasbd asbdkajbs KASBDKABS AJb a sduahvJHVJas dJ asjhJB jasjd jhabsd nashdhba</p>
         </div>
     
         <?php include 'signUp.php'; ?>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     </body>
     <?php include 'footer.html'; ?>
 </html>
